54b350ed7e0dbe6256299ac2dab1248db2d35355
GOLAM MOHAMMAD Shovon <38292502+golam-shovon@users.noreply.github.com>
2019-11-11 22:58:36 +0600

    second

--- elmo.py
+++ elmo.py
@@ -6,4 +6,34 @@ import re
 import time
 import pickle
 pd.set_option('display.max_colwidth', 200)
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
 
+train.shape, test.shape
+punctuation = '!"#$%&()*+-/:;<=>?@[\\]^_`{|}~'
+
+train['clean_tweet'] = train['clean_tweet'].apply(lambda x: ''.join(ch for ch in x if ch not in set(punctuation)))
+test['clean_tweet'] = test['clean_tweet'].apply(lambda x: ''.join(ch for ch in x if ch not in set(punctuation)))
+
+# convert text to lowercase
+train['clean_tweet'] = train['clean_tweet'].str.lower()
+test['clean_tweet'] = test['clean_tweet'].str.lower()
+
+# remove numbers
+train['clean_tweet'] = train['clean_tweet'].str.replace("[0-9]", " ")
+test['clean_tweet'] = test['clean_tweet'].str.replace("[0-9]", " ")
+
+# remove whitespaces
+train['clean_tweet'] = train['clean_tweet'].apply(lambda x:' '.join(x.split()))
+test['clean_tweet'] = test['clean_tweet'].apply(lambda x: ' '.join(x.split()))
+
+# import spaCy's language model
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+# function to lemmatize text
+def lemmatization(texts):
+    output = []
+    for i in texts:
+        s = [token.lemma_ for token in nlp(i)]
+        output.append(' '.join(s))
+    return output
